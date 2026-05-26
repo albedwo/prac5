252b58bf25b245e71df8dcbd373e549ac4801d48
unknown <angelinecalara3@gmail.com>
2026-05-26 13:50:17 +0800

    tapos na yung create index edit blade

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -21,20 +21,36 @@
                     <div class="alert alert-info">
                         This is the table for the students
                     </div>
+                    <a href="{{ route('student.create') }}" class="btn btn-primary mb-3">Add Student</a>
+                
 
                     <div class="card">
                         <div class="card-body p-0">
 
                             <table class="table">
                                 <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Age</th>
+                                        <th>Email</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                 {{-- @foreach($student) --}}
+                                 @foreach($students as $student)
                                     <tr>
-                                        <td>Matthew Bartolomeo</td>
-                                        <td>matthewbartolomeo@sample.com</td>
+                                        <td>{{ $student->name }}</td>
+                                        <td>{{ $student->age }}</td>
+                                        <td>{{ $student->email }}</td>
+
+                                        <td>
+                                            <a href="{{ route('student.edit', $student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                                            <form action="{{ route('student.destroy', $student->id) }}" method="POST" style="display:inline-block;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                                            </form>
                                     </tr>
-                                {{-- @endforeach --}}
+                                 @endforeach
                                 </tbody>
                             </table>
                         </div>
